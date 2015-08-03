464b9f3d8c4e0b1e733a7e1aabaf16cd35228686
Sugarcoder <mikichea@gmail.com>
2015-08-03 16:46:52 -0700

    hello, world!

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,12 @@
 public class Hello {
     public static void main (String[] args) {
         System.out.println("Hello, world!");
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
     }
 }
